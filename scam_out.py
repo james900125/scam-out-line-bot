349be1369e8226bdd69c8f556b8a99e42003ca4c
unknown <>
2017-05-30 22:50:03 +0800

    make it better

--- scam_out.py
+++ scam_out.py
@@ -44,14 +44,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-	if event.message.text == "詐騙奧特關閉".decode("utf-8") and event.source.userId not in switch:
+	if event.message.text == "詐騙奧特關閉".decode("utf-8") and event.Source.UserID not in switch:
 		switch.append(event.Source.UserID)
 		print("add", event.Source.UserID)
 		line_bot_api.reply_message(
 				event.reply_token,
 				TextSendMessage(text="詐騙奧特已關閉，請輸入「詐騙奧特開啟」啟動服務"))
 
-	elif event.message.text == "詐騙奧特開啟".decode("utf-8") and event.source.userId in switch:
+	elif event.message.text == "詐騙奧特開啟".decode("utf-8") and event.Source.UserID in switch:
 		switch.remove(event.Source.UserID)
 		print("remove", event.Source.UserID)
 		line_bot_api.reply_message(
