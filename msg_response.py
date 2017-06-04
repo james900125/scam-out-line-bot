60eb4beb7c6067a6574c40051239fa89718cc66a
unknown <>
2017-06-04 20:42:48 +0800

    make it better

--- msg_response.py
+++ msg_response.py
@@ -66,10 +66,8 @@ class Msg_response():
             query = ("SELECT content,original FROM data")#選取資料庫欄位
             cursor.execute(query)
             for content,original in cursor:
-                gov_data.append(content)
-                origin_data.append(original)
-            #with open('./json_data/Chen_family_2.json', encoding = 'utf8') as data_file: 
-                #original_data = json.load(data_file)
+                gov_data.append(content)  #政府澄清文
+                origin_data.append(original)  #謠言原文
             cursor.close()
             cnx.close()
             print("Connecting MySQL Successful!!!")
@@ -84,10 +82,10 @@ class Msg_response():
         jieba.analyse.set_idf_path('./archive/idf.txt.big')
         print(" jieba setup OK !!")
 
-    def data_prepare(self):
+    def data_prepare(self):  #compare text prepare  gov_original_message preprocessing
         seg_list2=[]
-        for index in range(len(self.gov_data)):
-            seg_list = jieba.analyse.extract_tags(self.gov_data[index], 100)	#取得前100個keyword 並去掉stopword
+        for index in range(len(self.origin_data)):
+            seg_list = jieba.analyse.extract_tags(self.origin_data[index], 100)	#取得前100個keyword 並去掉stopword
             seg_list2.append(seg_list)#斷詞加入list
         self.dictionary = gensim.corpora.Dictionary(seg_list2)
         corpus = [self.dictionary.doc2bow(gen_doc) for gen_doc in seg_list2]
@@ -95,9 +93,9 @@ class Msg_response():
         self.sims = gensim.similarities.Similarity('./json_data/', self.tf_idf[corpus], num_features = len(self.dictionary))
 
 
-    def msg_predict(self, msg):
+    def msg_predict(self, msg):   #define message information type
 
-        #msg = "新北市警察局土城分局警備隊~關心你！最近FB有一個測年齡的，那個不要去點他，因為只要你點下去，你的資料、照片、姓名…等等，都會給他們使用，請傳下去吧，別讓身邊周遭的朋友受害了！ 新北市警察局土城分局警備隊~關心你！(!?)新版 “身份證算命” 不要玩喔！ 請告訴你的好友要注意， 不法集團利用算命程式，收集身份證字號等資料，他們會要人輸入號碼後算出，千萬別玩，請轉貼分享出去！有毒 ！(NO)千萬別玩！"
+        #msg 放預分割字串 
         text_min = TextMining('archive/keyword_2.json')
         # Transforming string to numerical feature vector
         msg_feature_vector = text_min._make_ML_X([msg])
@@ -114,30 +112,10 @@ class Msg_response():
         return int(msg_class[0])
 
 
-    def compare(self, original_data):	#gov_data為list，需放入政府的資料；origin_data為list，需放入欲比對的資料；index為origin_data的index
+    def compare(self, original_data): #original_data需放入欲比對的字串
 
         #doc = jieba.analyse.extract_tags(original_data, 100)
         doc_bow = self.dictionary.doc2bow(jieba.analyse.extract_tags(original_data, 100))
         doc_tf_idf = self.tf_idf[doc_bow]
         result = numpy.argmax(self.sims[doc_tf_idf])
-        return result, self.sims[doc_tf_idf][result] * 100
-
-'''
-	s = 0
-	for i in corpus:
-		s += len(i)
-	query_doc=[]	
-	query_doc=jieba.analyse.extract_tags(origin_data[rumor_index], 100)	
-	#print (query_doc)#比對文章的斷詞
-	sims = gensim.similarities.Similarity('./json_data/',tf_idf[corpus],num_features=len(dictionary))#sim放置的路徑
-	query_doc_bow = dictionary.doc2bow(query_doc)
-	#print(query_doc_bow)
-	query_doc_tf_idf = tf_idf[query_doc_bow]
-	#print(query_doc_tf_idf)
-	#print(sims[query_doc_tf_idf])
-	#print("對應資料庫index:",numpy.argmax(sims[query_doc_tf_idf]))#
-	#print("對應相似度:",sims[query_doc_tf_idf][numpy.argmax(sims[query_doc_tf_idf])])
-	#print("政府原文: \n\n",gov_data[numpy.argmax(sims[query_doc_tf_idf])],"\n")
-	#print("放入比較原文: \n\n",origin_data[rumor_index])
-	return numpy.argmax(sims[query_doc_tf_idf]),sims[query_doc_tf_idf][numpy.argmax(sims[query_doc_tf_idf])]
-'''
+        return result, self.sims[doc_tf_idf][result] * 100  #回傳匹配結果
