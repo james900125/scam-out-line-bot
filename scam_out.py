1dec59be0639800ed07f03e280361a456c346a10
unknown <>
2017-06-04 19:59:28 +0800

    make it better

--- scam_out.py
+++ scam_out.py
@@ -10,15 +10,16 @@ from linebot.exceptions import (
 )
 from linebot.models import (
 	MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, StickerMessage, 
-	SourceUser, SourceGroup, SourceRoom, TemplateSendMessage, ButtonsTemplate
+	SourceUser, SourceGroup, SourceRoom
 )
 
 from msg_response import Msg_response
-#from msg_classifier_lg import msg_predict
+
+#### Data Prepare ###
 msg = Msg_response()
 msg.setup()
 msg.data_prepare()
-#print(str(msg.gov_data).encode("utf-8"))
+
 app = Flask(__name__)
 
 #line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
@@ -28,14 +29,13 @@ handler = WebhookHandler('38a504945e12d5a6bd5902af2ac3a4cf')
 
 switch = []
 
-
 #class_dic = {0:"chat", 1:"objective information", 2:"subjective information"}
 
 @app.route("/callback", methods=['POST'])
 def callback():
 	# get X-Line-Signature header value
 	signature = request.headers['X-Line-Signature']
-	#print("signature: ", signature)
+
 	# get request body as text
 	body = request.get_data(as_text=True)
 	app.logger.info("Request body: " + body)
@@ -50,78 +50,49 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-	if isinstance(event.source, SourceGroup):
+	if isinstance(event.source, SourceGroup):  #define MessageEvent from group/room/user
 		reply_id = event.source.group_id
+		type_id = "group"
 	elif isinstance(event.source, SourceRoom):
 		reply_id = event.source.room_id
+		type_id = "room"
 	else:
 		reply_id = event.source.user_id
+		type_id = "user"
+
+############## Turn on/off service operation #############
 
 	if event.message.text == "詐騙奧特關閉".decode("utf-8") and reply_id not in switch:
 		switch.append(reply_id)
-		print("add", reply_id)
+		print "add ID: {} in switch, the ID is {} type.".format(reply_id, type_id)
 		line_bot_api.reply_message(
 				event.reply_token,
 				TextSendMessage(text="詐騙奧特已關閉，請輸入「詐騙奧特開啟」啟動服務"))
 
 	elif event.message.text == "詐騙奧特開啟".decode("utf-8") and reply_id in switch:
 		switch.remove(reply_id)
-		print("remove", reply_id)
+		print "remove ID: {} from switch, the ID is {} type.".format(reply_id, type_id)
 		line_bot_api.reply_message(
 				event.reply_token,
 				TextSendMessage(text="詐騙奧特服務已啟動"))
 
+#############################################################
+
 	elif reply_id not in switch:
-		if msg.msg_predict(event.message.text) != 1: return 0
-		result, score = msg.compare(event.message.text)
+		if msg.msg_predict(event.message.text) != 1: return 0   #define message 0:chat / 1:objective / 2:subjective
+		result, score = msg.compare(event.message.text)   #searching message is in gov_api or not 
 		print(result, score)
-		if score > 20:
+		if score > 20:   #message is in gov_api
 			line_bot_api.reply_message(
 				event.reply_token,
-				TextSendMessage(text=u'此文章為謠言!\n政府澄清文:\n' + msg.gov_data[0]))
-
+				TextSendMessage(text=u'此文章為捏造的謠言!\n政府已有發出澄清文:\n' + msg.gov_data[0]))
 			return 0
+
+		#message is not in gov_api
 		line_bot_api.reply_message(
 				event.reply_token,
-				TextSendMessage(text="上述文章有潛在謠言嫌疑"))
+				TextSendMessage(text="此文章有潛在謠言嫌疑，請進行查證，避免上當受騙"))
 
-
-'''
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=str(result)))
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=str(score)))
-		if score >= 30: 
-			line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text=msg.gov_data[0]))
-'''
-'''
-	elif reply_id in switch:
-		line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text="詐騙奧特已關閉"))
-
-	elif reply_id not in switch:
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=class_dic[msg.msg_predict(event.message.text)]))
-'''
-'''
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=class_dic[msg.msg_predict(event.message.text)]))
-'''
-
-'''
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text="HA! HA! So funny"))
-'''
 
 if __name__ == "__main__":
 	app.run()
